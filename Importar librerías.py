b25a99738a06efe4d4d390c124d646c6f01d7d01
RaquelDH <149892539+RaquelDH@users.noreply.github.com>
2023-11-05 00:12:02 +0100

    Delete Importar librerías.py

--- Importar librerías.py
+++
@@ -1,7 +0,0 @@
-import pandas as pd
-
-# Lee el archivo CSV en un DataFrame
-df = pd.read_csv('Calidad_del_aire.csv')
-
-# Muestra las primeras filas (por defecto, muestra las primeras 5 filas)
-print(df.head())
